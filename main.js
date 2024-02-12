ecca286af8a68e2106acff78fcde23b66207cb2c
Kevin Liu <134642275+kcliu8035@users.noreply.github.com>
2024-02-12 01:50:17 -0500

    Added counter to limit characters shown in display. Functionality for decimal button to be only allowed once per number string.

--- main.js
+++ main.js
@@ -13,20 +13,8 @@ const mainDisplay = document.querySelector('.mainDisplay');
 const smallDisplay = document.querySelector('.smallDisplay');
 const numberButtons = document.querySelectorAll('.numberButtons');
 const operationButtons = document.querySelectorAll('.operationButton');
-
-
-numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        mainDisplay.innerHTML += button.textContent;
-    })
-});
-
-operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        smallDisplay.innerHTML += `${mainDisplay.textContent} ${button.textContent}`
-    })
-})
-
+const deleteBTN = document.getElementById('delete')
+const decimalBTN = document.getElementById('decimal');
 
 //OPERATIONS BUTTONS:
 const addBTN = document.getElementById('add');
@@ -36,22 +24,97 @@ const multiplyBTN = document.getElementById('multiply');
 const equalsBTN = document.getElementById('equals');
 const clearBTN = document.getElementById('clear');
 
+let firstInput = [];
+let secondInput = [];
+let calculation = [];
+
+let counter = 0;
+let decimalCounter = 0;
+
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        counter++;
+        if(counter <= 15) {
+            mainDisplay.innerHTML += button.textContent;
+            console.log(counter);
+        }
+        
+        if(counter >= 10) {
+            mainDisplay.style.fontSize = "3rem";
+        } else {
+            mainDisplay.style.fontSize = "4.5rem";
+        }
+    })
+    
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        counter = 0;
+        decimalBTN.disabled = false;
+        initiateCalculation();
+        smallDisplay.innerHTML += `${mainDisplay.textContent} ${button.textContent}`
+        mainDisplay.innerHTML = '';
+    })  
+})
+
+//NOTE: Had deleteBTN event listener nested in operationsButtons to target each button output
+deleteBTN.addEventListener('click', () => {
+    counter -= 1;
+    console.log(counter);
+    // mainDisplay.innerHTML += button.textContent.substring(-1, 1);
+})
+
+equalsBTN.addEventListener('click', () => {
+    mainDisplay.innerHTML = calculation;
+})
+
+decimalBTN.addEventListener('click', () => {
+   stopDecimal();
+})
+
+function stopDecimal() {
+    decimalBTN.disabled = true;
+}
+
+
+
+
 
 
 //OPERATIONS FUNCTIONALITIES:
 clearBTN.addEventListener('click', () => {
-    display.innerHTML = '';
+    counter = 0;
+    mainDisplay.innerHTML = '';
+    smallDisplay.innerHTML = '';
 });
 
 
+
 function sum (a, b) {
-    return a + b;
+    let input = a + b;
+    let input2 = a + b + 2;
+    firstInput.push(input);
+    secondInput.push(input2)
+    calculation.push(input + input2);
 }
+
+sum(1, 3);
+
+function initiateCalculation() {
+    console.log('initiated');
+};
 
 
 
 //NOTES:
     //1) Figured out how to have every button be targeted without having to define each one. May need to have button variable for every group.
-    //2) Need to figure out how to stop numbers from spilling out from display.  
-    //3) Include little display on calculator to show operations being used. 
-    //4) Figure out why Clear button stops anything from showing up again in display.
+    //2) (DONE) Need to figure out how to stop numbers from spilling out from display.  
+    //3) (DONE) Include little display on calculator to show operations being used. 
+    //4) (DONE) Figure out why Clear button stops anything from showing up again in display.
+    //5) Create math operation functions
+    //6) Figure out how to have two arrays calculate properly as integers. 
+    //7) firstInput is stored into array after operation button is clicked, and then secondInput is targeted.
+            //Any operation click leads to calculation into calculation.
+
