4a5ae3448543ef1333e03915daa1cf0d48e0c09d
Kevin Liu <134642275+kcliu8035@users.noreply.github.com>
2024-02-20 08:43:34 -0500

    Cut out some unnecessary code with counter attached to operators. Fixed bug where values weren't being stored properly for calculation.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const display = document.querySelector('.display');
-const mainDisplay = document.querySelector('.mainDisplay');
+let mainDisplay = document.querySelector('.mainDisplay');
 const smallDisplay = document.querySelector('.smallDisplay');
 const numberButtons = document.querySelectorAll('.numberButtons');
 const operationButtons = document.querySelectorAll('.operationButton');
@@ -17,14 +17,21 @@ const clearBTN = document.getElementById('clear');
 let firstInput = [];
 let secondInput = [];
 let calculation = [];
+let operatorInput = [];
 let counter = 0;
 let operationActivated = false;
 let operationsCounter = 0; 
+let numberButtonClicked = false;
 
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         counter++;
+        if (operationActivated) {
+            mainDisplay.innerHTML = '';
+        }
+        operationActivated = false;
+        numberButtonClicked = true;
         if(counter <= 15) {
             mainDisplay.innerHTML += button.textContent;
         }
@@ -40,28 +47,16 @@ numberButtons.forEach(button => {
 
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
-        
+        if(mainDisplay.textContent.length === 0) return;
         operationActivated = true;
-        if(operationActivated && operationsCounter === 2) {
-            initiateCalculation(firstInput, secondInput);
-            operationsCounter = 0;
-            firstInput = [];
-            secondInput = [];
-        }else if(operationActivated && operationsCounter === 1) {
-            clearDisplay = true;
-            secondInput = mainDisplay.textContent;
-            operationsCounter++;
-        } else if(operationActivated && operationsCounter === 0) {
-            firstInput = mainDisplay.textContent;
-            operationsCounter++;
-            calculation = [];
-        }
-        // console.log(operationActivated);
         counter = 0;
-        
         decimalBTN.disabled = false;
-        smallDisplay.innerHTML += `${mainDisplay.textContent} ${button.textContent}`
-        mainDisplay.innerHTML = '';
+        smallDisplay.innerHTML = `${calculation} ${button.textContent}`
+        // smallDisplay.innerHTML += `${mainDisplay.textContent} ${button.textContent}`
+        // if (operationActivated && numberButtonClicked) {
+        //     mainDisplay.innerHTML = '';
+        // }
+        
         console.log(`First: ${firstInput} \nSecond: ${secondInput} \nCalculation: ${calculation} \nCounter: ${operationsCounter}`);
        
     })  
@@ -71,6 +66,7 @@ operationButtons.forEach(button => {
 //OPERATIONS FUNCTIONALITIES:
 clearBTN.addEventListener('click', () => {
     counter = 0;
+    operationsCounter = 0;
     decimalBTN.disabled = false;
     mainDisplay.innerHTML = '';
     smallDisplay.innerHTML = '';
@@ -80,30 +76,14 @@ clearBTN.addEventListener('click', () => {
 });
 
 
-
-function sum (a, b) {
-    let input = a + b;
-    let input2 = a + b + 2;
-    firstInput.push(input);
-    secondInput.push(input2)
-    calculation.push(input + input2);
-}
-
-function initiateCalculation(input1, input2) {
-    // mainDisplay.innerHTML += calculated
-};
-
-
-
-//NOTE: Had deleteBTN event listener nested in operationsButtons to target each button output
 deleteBTN.addEventListener('click', () => {
-    counter -= 1;
-    console.log(counter);
-    // mainDisplay.innerHTML += button.textContent.substring(-1, 1);
+    mainDisplay.innerHTML = mainDisplay.textContent.slice(0, -1);
 })
 
 equalsBTN.addEventListener('click', () => {
-    mainDisplay.innerHTML = calculation;
+    let result = initiateCalculation(operatorInput);
+    // mainDisplay.innerHTML = calculation;
+    alert(`RESULT: ${result}, OPERATOR: ${operatorInput}, INPUT1: ${firstInput} INPUT2: ${secondInput}`);
 })
 
 decimalBTN.addEventListener('click', () => {
@@ -115,7 +95,7 @@ function stopDecimal() {
 }
 
 
-function operations(operator) {
+function initiateCalculation(operator) {
     var operators = {
         '+': function(a, b) {
             return a + b;
@@ -128,19 +108,35 @@ function operations(operator) {
         },
         '/': function(a, b) {
             return a / b;
-        },
+        }, 
     };
-    console.log(operators[operator](firstInput, secondInput));
+    
+
+
+    if(firstInput.length > 0) {
+        secondInput = mainDisplay.textContent;
+        firstInput = [operators[operatorInput](parseInt(firstInput), parseInt(secondInput))];
+        // alert(`RESULT: ${calculation}, OPERATOR: ${operatorInput}, INPUT1: ${firstInput} INPUT2: ${secondInput}`);
+        mainDisplay.innerHTML = firstInput;
+        secondInput = [];
+    } else if(firstInput.length === 0) {
+        firstInput = mainDisplay.textContent;
+    }
+    
+    operatorInput = operator;  
+    // debugger
+    
+   
 }
 
 
 
 //NOTES:
-    //1) Have Delete button delete numbers and numbers only.
+    //1) (DONE) Have Delete button delete numbers and numbers only.
     //2) (DONE) Need to figure out how to stop numbers from spilling out from display.  
     //3) (DONE) Include little display on calculator to show operations being used. 
     //4) (DONE) Figure out why Clear button stops anything from showing up again in display.
-    //5) Create math operation functions
+    //5) (DONE) Create math operation functions
     //6) Figure out how to have two arrays calculate properly as integers. 
     //7) firstInput is stored into array after operation button is clicked, and then secondInput is targeted.
             //Any operation click leads to calculation into calculation.
@@ -149,4 +145,27 @@ function operations(operator) {
             //If user continues calculations without resetting, then calculatedInput takes firstInput's place and is calculated with secondInput
                 //Which array do subsequent calculations then get stored? 
                 //EX: If(calculatedInput = true ? firstInput = calculatedInput)
+    //9) Need to find out how to have function immediately calculate after second input is identified and operation button is clicked again
+            // Problem seems to be in when the second input is stored into array. 
+            // Calculation does occur at the proper time, but second array is still empty when it is activated. 
+            // FIXED issue by moving storage of second input into the if function in initiateCalculation
+                //Issue now is need to implement the stored operatorInput value into calculation.
+    // 10) Create seperate function for equals operator that takes storedOperator and calls initiateCalculation().
+            //if (smallDisplay !== '') if it's not empy, execute calculation.
+    // 11) Make sure 0 or symbols cannot be first input.
+            // if(mainDisplay = '0' || 'operator') 
+    // 12) Need to have it so only one operator can be chosen at a time. Currently, operationCounter can increment continuously. 
+            // .includes(operator) return
+    // 13) Need to use parseFloat when implementing decimals.
+    // 14) .slice(0, -1) for delete operator
+    // 15) to add commas, create function to return string as number (can parseFloat it first to account for decimals)
+    // 16) for decimals, use .split() to split sequence on the decimal.
+            // Look up maximumFractionDigits for other method to prevent more than one decimal.
+            // Other method is to create function checking if current input contains a decimal and if not to add it to the input.
+
+    //17) (DONE)Figure out booleans for when you click on operator and then clck on preceding number
+            // Just changed order in which mainDisplay textContent was cleared
+    //18) Problem with Equals button calculating firstInput and secondInput and storing it into secondInput:
+            // secondInput is defined after clicking an operator
+            
 
